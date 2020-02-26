26de140658ddd92c13b58c8bc446402a0dff4c9c
sunxiaobin <ulxwork@gmail.com>
2020-02-26 09:23:59 +0800

    clear cached images everyday

--- movietv/tasks.py
+++ movietv/tasks.py
@@ -1,12 +1,27 @@
 import logging
+import glob
+import pathlib
+from datetime import datetime, timedelta
 from celery import task, shared_task
 from django.test import Client
 
 logger = logging.getLogger('movietv')
 c = Client()
 
+def purge_cache_images(s):
+    today = datetime.today()
+    yesterday = today - timedelta(days=1)
+    fil = 'images/' + yesterday.strftime('%Y-%m-%d') + '-' + s + '*'
+    path = '/home/sun/src/web/douban/movietv/static/' + fil
+    #path = settings.STATICFILES_DIRS[0] + fil
+
+    for p in glob.glob(path):
+        pathlib.Path(p).unlink()
+
 @task(name='hottest')
 def generate_hottest():
+    purge_cache_images('hottest')
+
     url = '/movietv/statis/hottest/'
     recent = [3, 5, 10, 15, 30]
     rdate = [5, 10, 15, 30, 60, 90, 180, 365]
@@ -19,6 +34,8 @@ def generate_hottest():
 
 @task(name='regionbar')
 def generate_regionbar():
+    purge_cache_images('regionbar')
+
     url = '/movietv/statis/regionbar/'
     tp = ['剧情', '喜剧', '动作', '爱情', '科幻', '动画', '悬疑',
           '惊悚', '恐怖', '犯罪', '同性', '音乐', '歌舞', '传记',
@@ -39,6 +56,8 @@ def generate_regionbar():
 
 @task(name='typebar')
 def generate_typebar():
+    purge_cache_images('typebar')
+
     url = '/movietv/statis/typebar/'
     edate = [5, 10, 15, 30, 60, 90, 120, 180, 200, 250,
              300, 365, 500, 600, 700, 800, 900, 1000]
